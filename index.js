cbf20ac51430b0c93fa808e3af1be4b91026f9d7
Sang Nguyen <75923274+ssangnd@users.noreply.github.com>
2023-06-02 22:21:53 +0700

    add src

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+var mysql = require('mysql');
+const app = express();
+const post = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello this is sample server');
+});
+app.get('/mysql', (req, res) => {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "sangnd",
+        password: "123456"
+    });
+    con.connect(function (err) {
+        if (err) {
+            res.send('Connection error: ' + err);
+            return;
+        }
+
+        con.query("SHOW DATABASES;", function (err, result) {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+})
+app.listen(post, (req, res) => {
+    console.log(`Example app listening on port ${post}!`);
+})
